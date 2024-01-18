6905ff9ce80a4e7cb903e2e218b70cb63fbde7d7
mevipinmaurya <mevipin2212001@gmail.com>
2024-01-18 18:45:03 +0530

    Added Searching Feature

--- app.js
+++ app.js
@@ -223,3 +223,105 @@ const ratingColor = (rating) => {
     else
         return 'red';
 }
+
+
+const genreTags = document.getElementById("scrollbox-inner");
+
+const genre = BASE_URL + "/genre/movie/list?" + API_KEY;
+
+const selectGenre = [];
+
+const getGenre = async (url) => {
+    let response = await fetch(url);
+    let data = await response.json();
+    setGenre(data.genres);
+}
+
+getGenre(genre);
+
+
+const setGenre = (data) => {
+    genreTags.innerHTML = "";
+    const t = document.createElement('div');
+    t.classList.add('tag', 'genre');
+    t.innerText = "Genre";
+    genreTags.append(t);
+    data.forEach(genre => {
+        const tg = document.createElement("div");
+        tg.id = genre.id;
+        tg.classList.add('tag');
+        tg.innerText = genre.name;
+        genreTags.append(tg);
+    })
+}
+
+
+const search = document.getElementById("search");
+const form = document.getElementById("form");
+const catMovies = document.getElementById("categories-movies")
+const moviesContainer = document.getElementById("movies-container")
+const searchMovieCont = document.getElementById("search-movie-cont")
+
+
+const getSearchMovies = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.results.length != 0) {
+        showSearchMovies(data.results);
+    }
+    else {
+        catMovies.classList.add('disp-none');
+        moviesContainer.classList.remove('disp-none');
+        searchMovieCont.innerText = "";
+        moviesContainer.innerHTML = `<h1 style="color:white;"> No Results Found </h1>`
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // console.log(search.value);
+    let searchTerm = search.value;
+
+    if (searchTerm) {
+        getSearchMovies(SEARCH_URL + "&query=" + searchTerm);
+    }
+})
+
+const showSearchMovies = (data) => {
+    catMovies.classList.add('disp-none');
+    moviesContainer.classList.remove('disp-none');
+    searchMovieCont.innerText = "";
+    data.forEach(movie => {
+        const { title, poster_path, vote_average, release_date } = movie;
+        let newTitle = "";
+        if (title.length >= 12) {
+            let titleArr = title.split(" ");
+            newTitle = titleArr.splice(0, 3).join(" ").concat(".....");
+        }
+        else {
+            newTitle = title;
+        }
+        const newEl = document.createElement('div');
+        newEl.classList.add('box', 'column');
+        newEl.innerHTML = `
+            
+                <div class="img">
+                    <img src="${poster_path ? IMG_URL + poster_path : "http://via.placeholder.com/200x230"}" alt="${title}">
+                </div>
+                <div class="movie-details">
+                    <div class="movie-name">
+                        <p>${newTitle}</p>
+                    </div>
+                    <div class="release-rating">
+                        <div class="rating">
+                            <p><i class="fa-solid fa-star ${ratingColor(vote_average)}"></i> ${vote_average.toFixed(1)}</p>
+                        </div>
+                        <div class="release-year">
+                            <p>${release_date}</p>
+                        </div>
+                    </div>
+                </div>
+        `
+        searchMovieCont.appendChild(newEl);
+    });
+}
